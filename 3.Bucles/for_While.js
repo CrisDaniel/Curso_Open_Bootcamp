76b434aaf94ca7c30289c5bd89966202a706ad86
Cristian <daniel981311@gmail.com>
2023-03-04 20:03:53 -0500

    Actualizacion archivo

--- 3.Bucles/for_While.js
+++ 3.Bucles/for_While.js
@@ -4,21 +4,21 @@ El programa debe tener las siguientes características:
 -El programa debe contar tanto las vocales en mayúsculas como en minúsculas.
 -El programa debe imprimir la cantidad de vocales que se encontraron en el string.*/
 
-let string = "HolA mundo";
+let string = "Web";
 let contador = 0;
+let vocales = ["a","e","i","o","u"];
 
 function cantidadVocales(){
-    for (let index = 0; index < string.length; index++) {
+    for (let letras = 0; letras < string.length; letras++) {
         cadena = string.toLocaleLowerCase();
-        console.log(cadena[index]);
-        if(cadena[index] == "a"||cadena[index] == "e"||cadena[index] == "i"||cadena[index] == "o"||cadena[index] == "u"){
-            contador++;
-        }
-        else{
-            console.log("No existe vocales");
+        for (let vocal = 0; vocal < vocales.length; vocal++) {
+            if (string[letras] == vocales[vocal]) {
+                contador++;
+            };
+            
         }
     }
-    return contador;
+    return contador || "Ninguna";
 }
 
-console.log(cantidadVocales());
+console.log("Vocales encontradas: " + cantidadVocales());
