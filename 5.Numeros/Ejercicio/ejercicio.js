790409491466b6e7947419bfbcd84d6bb7556c54
Cristian <daniel981311@gmail.com>
2023-03-11 16:16:23 -0500

    Ejercicio

---
+++ 5.Numeros/Ejercicio/ejercicio.js
@@ -0,0 +1,11 @@
+let cmAltura = 156;
+let mAltura = 1.56;
+let kPeso = 42.25;
+
+let mAlturaEntero = Math.round(mAltura);
+let kPesoEntero = parseInt(kPeso);
+let maxValorMasUno = Number.MAX_VALUE === (Number.MAX_VALUE +1);
+
+console.log(mAlturaEntero);
+console.log(kPesoEntero);
+console.log(maxValorMasUno);
