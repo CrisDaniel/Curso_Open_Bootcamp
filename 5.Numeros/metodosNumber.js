398887daa486ad6ee00c09fdeef5245c42537b45
Cristian <daniel981311@gmail.com>
2023-03-11 15:57:46 -0500

    modificado

--- 5.Numeros/metodosNumber.js
+++ 5.Numeros/metodosNumber.js
@@ -1,3 +1,4 @@
+//valueOF()
 let a = 8;
 let b = new Number(9);
 
@@ -9,3 +10,46 @@ console.log("b: " + b.valueOf());
 let str = new String("Soy un string");
 console.log(str);
 console.log(str.valueOf());
+console.log("")
+
+//NaN (Not a Number) - Infinity
+
+let noNumber = new Number("hola");
+console.log(noNumber.valueOf());
+console.log(isNaN(noNumber));       //isNaN retorna un true si el parametro no es un numero y false si lo es;
+
+let x = 10;
+let z = 0;
+
+console.log(x/z);
+
+// De string a number - Number, parseInt y parseFloat
+
+let numeroString = "25";
+let num = 45;
+
+let convertirParseInt = parseInt(numeroString);     //parseInt si agarra un decimal lo que hace es tomar solo la parte entera
+let convertirNumber = Number(numeroString);
+let convertirParseFloat = parseFloat(numeroString); //parseFloat agarra tambien los decimales
+
+let sumaConvertida = num + convertirParseFloat;
+
+console.log(sumaConvertida);
+console.log("");
+
+// Numeros hexadecimales (base 16)
+
+let numHex = "0xaf5c7";
+
+console.log(parseInt(numHex, 16)); //Imprimir un numero hexadecimal (entre parentesis se puede omitir el 16, y si se quiere en base 8 cambiamos a (numeroHex, 8))
+
+//Obtener los valores max y min en JavaScript
+
+let min_presicion = Number.EPSILON;;
+let minValorJs = Number.MIN_VALUE;
+let maxValorJs = Number.MAX_VALUE;
+
+console.log(min_presicion);
+console.log(minValorJs);
+console.log(maxValorJs);
+console.log(2 ** 1024);
