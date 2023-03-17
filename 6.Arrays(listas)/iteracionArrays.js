76abcb149a39e82b33a9602ca8965e5d482ffb4c
Cristian <daniel981311@gmail.com>
2023-03-16 19:16:30 -0500

    Actualizacion

--- 6.Arrays(listas)/iteracionArrays.js
+++ 6.Arrays(listas)/iteracionArrays.js
@@ -10,6 +10,40 @@ console.log(".......................");
 
 //Metodo forEch
 
-array.forEach(iterador => {
-    console.log(iterador)
+array.forEach(valor => {
+    console.log(valor)
 });
+console.log("--------------------------");
+
+//Busqueda de un valor dentro de una lista con .find() que ejecuta la función callback una vez por cada índice del array
+//hasta que encuentre uno en el que el callback devuelva un valor verdadero. Si es así, find devuelve inmediatamente el
+//valor del elemento. En caso contrario, find devuelve undefined
+
+let busqueda = array.find(elemento => {
+    if(elemento === "Como estas"){
+        return  "Si hay";
+    };
+});
+console.log(busqueda);
+console.log("***************************")
+
+let listaObjetos = [
+    {nombre: "Eleonor", edad: 14},
+    {nombre: "Lian", edad: 22},
+    {nombre: "Ian", edad: 22},
+    {nombre: "Ciel", edad: 4},
+    {nombre: "Moenia", edad: 18},
+    {nombre: "Athena", edad: 5}
+]
+
+let nombres = listaObjetos.find(hijos => {
+    if (hijos.edad == 5) {
+        return true;
+    }
+});
+console.log(nombres);
+
+let objeto = listaObjetos.find(age => age.edad === 18);
+console.log(objeto.nombre);
+let {edad} = listaObjetos.find(age => age.nombre === "Eleonor");
+console.log(edad);
