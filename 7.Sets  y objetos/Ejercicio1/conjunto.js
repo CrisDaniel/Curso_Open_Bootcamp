904a19bc77e17a0fb0ad3739f602fc01d0e51e1d
Cristian <daniel981311@gmail.com>
2023-03-30 16:28:13 -0500

    Ejercicio 1

---
+++ 7.Sets  y objetos/Ejercicio1/conjunto.js
@@ -0,0 +1,9 @@
+const familia = ["Cristian", "Victor", "Victor", "Eduardo", "Liliana"];
+let nameFamilia = new Set(familia);
+console.log(nameFamilia);
+
+nameFamilia.add("Cristian");
+console.log(nameFamilia);
+
+nameFamilia.add("Java Script");
+console.log(nameFamilia);
