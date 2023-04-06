a97074cbebb24a8029eb9249d768b52f8a59bd1b
Cristian <daniel981311@gmail.com>
2023-04-06 18:48:31 -0500

    Funcion generadora

---
+++ 8.Funciones/funcionesGeneradoras.js
@@ -0,0 +1,24 @@
+function* generarID(){  //function*() => es una funcion feneradora(iteradora)
+    let id = 0;
+    while(true){
+        id++;
+        if(id===10){
+            return id;
+        }
+        yield id;   //Devuelve el id y espera hasta que se llamada nuevamente.
+    }
+}
+
+let generadora = generarID();
+
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
+console.log(generadora.next());
