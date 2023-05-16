d63d41c6f91e12f994959e92713c45735ec76f61
Cristian <daniel981311@gmail.com>
2023-05-16 01:41:50 -0500

    Reglas temporales y scrips para ejecucion de ESLint en el codigo

--- 13.Configuracion y uso de EsLint/slint-npm/index.js
+++ 13.Configuracion y uso de EsLint/slint-npm/index.js
@@ -1,4 +1,24 @@
-let nombre = "Golria";
+let nombre = "Gloria";
+
+/* eslint-disable */
+const persona = 'Angel';    //Desactivamos eslint para que funcione las comillas simples y la ","
+
+/* eslint-enable */
 
 //En esta linea quiero que descatives la comillas dobles y usar comillas simples
-//let persona = 'humano';
+let persona1 = 'humano'; //eslint-disable-line
+
+/* eslint-disable-next-line- indent */
+const string = "Hola como estas";
+
+console.log("Hola");
+
+let nombre2 = "Gloria";
+
+console.log(23);
+
+//En la terminal para mostrar cuantos y que errores hay se usa el comando:
+//npm run lint
+
+//Y para corregir algunos errores automaticamente el comando:
+//npm run lint-fix
