54f45feef76553f3c8a7d5f9d913da3f00e04aa9
Cristian <daniel981311@gmail.com>
2023-06-03 15:48:55 -0500

    Mi ubicacion con Geolocalizacion en Js

--- 17.Geolocalizacion/index.js
+++ 17.Geolocalizacion/index.js
@@ -1,12 +1,47 @@
+let map, marker;
+
 function initMap(){
-    //Localizacion de Uluru
+    //Localizacion de Tocache
     const posicion = {
-        lat: -25.344, 
-        lng: 131.031
+        lat: -8.18861, 
+        lng: -76.5103
     };
 
-    const map =new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+    map =new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
         center: posicion
     });
+
+    marker = new google.maps.Marker({
+        position: posicion,
+        map,
+        title: "Ubicacion de tocache"
+    });
+
+    geoPosicion();
+}
+
+function geoPosicion(){
+    if(navigator.geolocation){
+        const geoLoc = navigator.geolocation;
+        const options = {timeout: 60000};
+        const watchPosicion = geoLoc.watchPosition(centrarMapa, onError, options);
+    }else{
+        alert("No admite navigator.geolocation");
+    };
+}
+
+function centrarMapa(position){
+    let nuevaPosicion = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    }
+    console.log(nuevaPosicion + "o");
+    marker.setPosition(nuevaPosicion);
+    map.setCenter(nuevaPosicion);
+}
+
+function onError(error){
+    console.log("Se ha producido un error, Active su ubicacion");
+    console.error(error);
 }
